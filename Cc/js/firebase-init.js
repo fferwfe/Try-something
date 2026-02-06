c7c944ee9e52e61cad77d0e60da90584dc0bfa81
fferwfe <57056385+fferwfe@users.noreply.github.com>
2026-02-06 20:14:07 +0800

    Add files via upload

---
+++ Cc/js/firebase-init.js
@@ -0,0 +1,5 @@
+// js/firebase-init.js
+firebase.initializeApp({
+  databaseURL: "https://try-something-ddd1e-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+window.db = firebase.database();
