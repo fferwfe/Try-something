36a06df20933ba1bfd74bc0355fd2d231053f824
fferwfe <57056385+fferwfe@users.noreply.github.com>
2026-03-18 00:50:08 +0800

    Delete 57 directory

--- 57/js/firebase-init.js
+++
@@ -1,5 +0,0 @@
-// js/firebase-init.js
-firebase.initializeApp({
-  databaseURL: "https://try-something-ddd1e-default-rtdb.asia-southeast1.firebasedatabase.app"
-});
-window.db = firebase.database();
